128bf763d4124d6d83e476c43d751995a901d22b
zoo <anfdnfl@khu.ac.kr>
2025-03-14 21:31:28 +0900

    login

--- src/models/User.js
+++ src/models/User.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
     fastFood: { type: Boolean, default: true },
     cafe: { type: Boolean, default: true },
   },
-  cashbackStamps: { type: [Number], default: [] }, // 스탬프(100 or 500) 값 저장
+  cashbackStamps: { type: [Number], default: [100, 500, 100, 500, 100, 500, 100, 500, 100, 500] }, // 스탬프(100 or 500) 값 저장
   cashback: { type: mongoose.Schema.Types.ObjectId, ref: "Cashback" }, // 캐시백 관련 정보
   donate: { type: mongoose.Schema.Types.ObjectId, ref: "Donate" }, // 기부 관련 정보
   invest: { type: mongoose.Schema.Types.ObjectId, ref: "Invest" }, // 투자 관련 정보
